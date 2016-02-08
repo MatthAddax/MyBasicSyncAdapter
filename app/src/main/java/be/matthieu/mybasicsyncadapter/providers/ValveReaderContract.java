919f433eb143b58491060595173e726f59b1b129
MatthAddax <matth.meun@hotmail.com>
2016-02-08 14:53:14 +0100

    ajout content provider

--- app/src/main/java/be/matthieu/mybasicsyncadapter/providers/ValveReaderContract.java
+++ app/src/main/java/be/matthieu/mybasicsyncadapter/providers/ValveReaderContract.java
@@ -9,6 +9,7 @@ public class ValveReaderContract {
     /*Definition de la table*/
     public static abstract class ValvesEntry implements BaseColumns {
         public static final String TABLE_NAME = "valves";
+        public static final String COLUMN_NAME_ID = "_id";
         public static final String COLUMN_NAME_VALVE_TITLE = "valve_title";
         public static final String COLUMN_NAME_VALVE_CONTENT = "valve_content";
     }
